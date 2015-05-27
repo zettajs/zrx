29de77953ae2267a108c396dc538dfba95f209b3
Matthew Dobson <mdobson4@gmail.com>
2015-05-27 12:00:00 -0400

    Removed peer rel search.

--- zrx.js
+++ zrx.js
@@ -25,9 +25,7 @@ Zrx.prototype.peers = function(name) {
     .link('http://rels.zettajs.io/server', name)
     .catch(Rx.Observable.empty());
 
-  var peer = siren().load(this.root).link('http://rels.zettajs.io/peer', name);
-
-  return new Zrx(server.merge(peer));
+    return new Zrx(server);
 };
 
 Zrx.prototype.query = function(ql) {
